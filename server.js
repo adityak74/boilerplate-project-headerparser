6301292b8731d632d4ef373d21e95d3197bdcfcd
Aditya Karnam <akarnam37@gmail.com>
2021-07-10 01:48:49 -0500

    feat: add whoami route

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress, language, software });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
